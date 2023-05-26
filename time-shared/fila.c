b59931e0d49fae8d35659845b4f4baf62f251558
Esdras Silva <esdraslimasilva83@gmail.com>
2023-05-26 07:29:32 -0300

    Add system initializer and basic control

---
+++ time-shared/fila.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "processo.h"
+#include "fila.h"
+
+struct nodo
+{
+    Processo *pProcesso;
+    Nodo *pProximo;
+};
+
+struct fila
+{
+    Nodo *pHead;
+};
+
+Fila *criarFila()
+{
+    Fila *pFila = (Fila *)malloc(sizeof(Fila));
+    if (!pFila)
+        return NULL;
+
+    pFila->pHead = NULL;
+
+    return pFila;
+}
+
+int inserir(Processo *pProcesso, Fila *pFila)
+{
+    if (!pProcesso || !pFila)
+        return 0;
+
+    Nodo *pNodo = (Nodo *)malloc(sizeof(Nodo));
+    if (!pNodo)
+        exit(EXIT_FAILURE);
+
+    pNodo->pProximo = NULL;
+    pNodo->pProcesso = pProcesso;
+
+    if (!pFila->pHead)
+    { // lista vazia
+
+        pFila->pHead = pNodo;
+        return 1;
+    }
+
+    // inserindo o nodo no fim da fila
+    Nodo *pAux = pFila->pHead;
+    while (pAux->pProximo)
+        pAux = pAux->pProximo;
+
+    pAux->pProximo = pNodo;
+    return 1;
+}
+
+void mostrarFila(Nodo *pNodo)
+{
+    if (pNodo)
+        mostrarProcesso(pNodo->pProcesso);
+
+    if (pNodo->pProximo)
+        mostrarFila(pNodo->pProximo);
+}
+
+Nodo *pegarHead(Fila *pFila)
+{
+    return pFila->pHead;
+}
--- time-shared/main.c
+++ time-shared/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <locale.h>
 
-#include "processo.h"
+#include "sistema-dinamico.h"
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    Processo *pProcesso = criarProcesso("O melhor processo do mundo");
-    mostrarProcesso(pProcesso);
+    Sistema *pSistema = inidicarSistema();
+
+    adicionarProcesso(pSistema, "Processo 1");
+    adicionarProcesso(pSistema, "Processo 2");
+    adicionarProcesso(pSistema, "Processo 3");
+    adicionarProcesso(pSistema, "Processo 4");
+
+    mostrarProcessos(pSistema);
 }
